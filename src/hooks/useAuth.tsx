e9840af5f7db63dff7f53b5ef788dce6b36104bd
ShimpeiHashizume <69233878+ShimpeiHashizume@users.noreply.github.com>
2022-09-03 11:38:30 +0900

    Initial commit

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
+import { useLoginUser } from "../hooks/useLoginUser";
+
+export const useAuth = () => {
+  const history = useHistory();
+  const { showMessage } = useMessage();
+  const { setLoginUser } = useLoginUser();
+
+  const [loading, setLoading] = useState(false);
+
+  const login = useCallback(
+    (id: string) => {
+      setLoading(true);
+      axios
+        .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => {
+          if (res.data) {
+            const isAdmin = res.data.id === 10 ? true : false;
+            setLoginUser({ ...res.data, isAdmin });
+            // データ取得できた場合
+            showMessage({ title: "ログインしました", status: "success" });
+            history.push("/home");
+          } else {
+            // データ取得できなかった場合
+            showMessage({ title: "ユーザーが見つかりません", status: "error" });
+            setLoading(false);
+          }
+        })
+        // エラーの場合
+        .catch(() => {
+          showMessage({ title: "ログインできません", status: "error" });
+          setLoading(false);
+        });
+    },
+    [history, showMessage, setLoginUser]
+  );
+  return { login, loading };
+};
